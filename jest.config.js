d730fc59b1b9e0ce9d96b23d2b867899e4bde017
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-03-14 20:42:26 -0300

    chore: config webpack

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,16 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}'
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
-  testEnvironment: 'js-dom',
+  testEnvironment: 'jsdom',
   transform: {
     '.+\\.(ts|tsx)$': 'ts-jest'
   },
   moduleNameMapper: {
-    '@/(.*)': '<rootDir>/src/$1'
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy'
   }
 }
